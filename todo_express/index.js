f20ef5d00ada4bb146d303da19ed4dba6323d187
Bruno <bruno.rozov@voco.ee>
2026-09-08 09:43:30 +0000

    linx reset

--- todo_express/index.js
+++ todo_express/index.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+
 
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-  res.render('index')
-})
+    fs.readFile('tasks.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+        console.timeLogger('data')
+        const tasks = JSON.parse(data)
+        res.render('index', { tasks })
+    })
+})          ._router,d[[f
+
 
 app.listen(1568, () => {
   console.log('Server running on port 1568')
